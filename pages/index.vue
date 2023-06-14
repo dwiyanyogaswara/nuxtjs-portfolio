6fe84cb5352cb64436fad80dd38d124b5a91955c
DESKTOP-2M8HV2R\user <dwiyanyogaswara@gmail.com>
2023-06-14 13:19:37 +0700

    Configure Tailwind CSS in Nuxt 3

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,11 @@
 <template>
-    <div>
-halo
-    </div>
+  <div>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </div>
 </template>
 
 <script setup>
-
 </script>
 
 <style lang="scss" scoped>
-
 </style>
