9799ecce9a0194a73abecc7ea848247b13077ad9
DESKTOP-2M8HV2R\user <dwiyanyogaswara@gmail.com>
2023-06-15 10:04:44 +0700

    test update netlify

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,76 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div class="container mx-auto">
+      <nav class="container flex justify-around py-8 mx-auto bg-white">
+        <div>
+          <h3 class="text-2xl font-medium text-blue-500">LOGO</h3>
+        </div>
+        <div class="space-x-8">
+          <a href="#home">Home</a>
+          <a href="#about">About Us</a>
+          <a href="#features">features</a>
+          <a href="#team">Our Team</a>
+        </div>
+      </nav>
+      <main>
+        <section id="about" class="mb-8 scroll-mt-20">
+          <h1 class="mb-4 text-2xl font-bold text-center">About Section</h1>
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+          </div>
+        </section>
+        <section id="features" class="mb-8 scroll-mt-20">
+          <h1 class="mb-4 text-2xl font-bold text-center">Features Section</h1>
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+          </div>
+        </section>
+        <section id="team" class="mb-8 scroll-mt-20">
+          <h1 class="mb-4 text-2xl font-bold text-center">Our Team Section</h1>
+          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+            <img class="object-cover w-full border"
+              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png" />
+          </div>
+        </section>
+      </main>
+    </div>
   </div>
 </template>
 
 <script setup>
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 </style>
