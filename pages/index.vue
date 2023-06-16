2ce249126b10d0008768f711006c1f76ea0f4974
DESKTOP-2M8HV2R\user <dwiyanyogaswara@gmail.com>
2023-06-16 15:55:17 +0700

    skills

--- pages/index.vue
+++ pages/index.vue
@@ -41,6 +41,63 @@
             </div>
           </div>
         </section>
+        <section id="skills" class="mb-8 scroll-mt-20">
+          <h1 class="mb-4 text-2xl font-bold text-center">
+            Our skills Section
+          </h1>
+          <div
+            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-500 to-blue-900"
+          >
+            <h3 class="mb-4 text-2xl font-bold text-center">Mobile</h3>
+            <div class="grid grid-cols-1">
+              <div class="p-2 mx-auto"><img src="assets\images\flutter.png" alt=""></div>
+            </div>
+          </div>
+          <div
+            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-500 to-blue-900"
+          >
+            <h3 class="mb-4 text-2xl font-bold text-center">Frontend</h3>
+            <div class="grid grid-cols-5">
+              <div class="p-2"><img src="assets\images\vue.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\nuxt.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\asp.jpg" alt=""></div>
+              <div class="p-2"><img src="assets\images\bootstrap.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\tailwind.png" alt=""></div>
+            </div>
+          </div>
+          <div
+            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-500 to-blue-900"
+          >
+            <h3 class="mb-4 text-2xl font-bold text-center">Backend</h3>
+            <div class="grid grid-cols-4">
+              <div class="p-2"><img src="assets\images\express.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\laravel.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\codeigniter.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\csharp.png" alt="aa"></div>
+            </div>
+          </div>
+          
+          <div
+            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-500 to-blue-900"
+          >
+            <h3 class="mb-4 text-2xl font-bold text-center">Database</h3>
+            <div class="grid grid-cols-5">
+              <div class="p-2"><img src="assets\images\mysql.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\postgresql.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\mssql.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\oracle.png" alt=""></div>
+              <div class="p-2"><img src="assets\images\mongodb.png" alt=""></div>
+            </div>
+          </div>
+          <div
+            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-500 to-blue-900"
+          >
+            <h3 class="mb-4 text-2xl font-bold text-center">Dev Ops</h3>
+            <div class="grid grid-cols-1">
+              <div class="p-2 mx-auto"><img src="assets\images\docker.png" alt=""></div>
+            </div>
+          </div>
+        </section>
         <section id="work" class="mb-8 scroll-mt-20">
           <h1 class="mb-4 text-2xl font-bold text-center">About Section</h1>
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
@@ -72,37 +129,6 @@
         </section>
         <section id="portfolio" class="mb-8 scroll-mt-20">
           <h1 class="mb-4 text-2xl font-bold text-center">portfolio Section</h1>
-          <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
-            <img
-              class="object-cover w-full border"
-              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png"
-            />
-            <img
-              class="object-cover w-full border"
-              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png"
-            />
-            <img
-              class="object-cover w-full border"
-              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png"
-            />
-            <img
-              class="object-cover w-full border"
-              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png"
-            />
-            <img
-              class="object-cover w-full border"
-              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png"
-            />
-            <img
-              class="object-cover w-full border"
-              src="https://cdn.pixabay.com/photo/2018/05/08/14/34/lightweight-transporter-3383177__340.png"
-            />
-          </div>
-        </section>
-        <section id="skills" class="mb-8 scroll-mt-20">
-          <h1 class="mb-4 text-2xl font-bold text-center">
-            Our skills Section
-          </h1>
           <div class="grid grid-cols-1 gap-4 lg:grid-cols-3 sm:grid-cols-2">
             <img
               class="object-cover w-full border"
