46634fb92fc28e39edd408616493a9efff3669ce
DESKTOP-2M8HV2R\user <dwiyanyogaswara@gmail.com>
2023-06-21 17:52:21 +0700

    fixing skills

--- pages/index.vue
+++ pages/index.vue
@@ -21,16 +21,34 @@
             >
               <div class="flex flex-row content-between">
                 <div class="p-2 w-[200px] h-[200px] hover:animate-bounce">
-                  <img src="assets\images\linkedin-icon.png" alt="" />
+                  <a
+                    href="https://www.linkedin.com/in/dwiyan-yogaswara"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src="assets\images\linkedin-icon.png" alt=""
+                  /></a>
                 </div>
                 <div class="p-2 w-[200px] h-[200px] hover:animate-bounce">
-                  <img src="assets\images\whatsapp.png" alt="" />
+                  <a
+                    href="https://wa.me/6285797736066"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    ><img src="assets\images\whatsapp.png" alt=""
+                  /></a>
                 </div>
                 <div class="p-2 w-[200px] h-[200px] hover:animate-bounce">
-                  <img src="assets\images\github.png" alt="" />
+                  <a
+                    href="https://github.com/dwiyanyogaswara"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    ><img src="assets\images\github.png" alt=""
+                  /></a>
                 </div>
                 <div class="p-2 w-[200px] h-[200px] hover:animate-bounce">
-                  <img src="assets\images\CV.png" alt="" />
+                  <a href="assets\files\CV 2023 Dwiyan Yogaswara.pdf" download
+                    ><img src="assets\images\CV.png" alt=""
+                  /></a>
                 </div>
               </div>
             </div>
@@ -38,81 +56,92 @@
         </section>
         <section id="skills" class="mb-8 scroll-mt-20">
           <h1 class="mb-4 text-2xl font-bold text-center">My Skills</h1>
-          <div
-            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
-          >
-            <h3 class="mb-4 text-2xl font-bold text-center">Mobile</h3>
-            <div class="grid grid-cols-1">
-              <div class="p-2 mx-auto">
-                <img src="assets\images\flutter.png" alt="" />
+          <div class="grid grid-cols-3 gap-8">
+            <div
+              class="col-span-1 rounded-lg shadow-md p-6 mb-5 h-fit bg-gradient-to-r from-blue-400 to-blue-900"
+            >
+              <h3 class="mb-4 text-2xl font-bold text-center">Mobile</h3>
+              <div class="grid grid-cols-1">
+                <div class="p-2 mx-auto w-[200px] h-fit">
+                  <img src="assets\images\flutter.png" alt="" />
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
-          >
-            <h3 class="mb-4 text-2xl font-bold text-center">Frontend</h3>
-            <div class="grid grid-cols-5">
-              <div class="p-2"><img src="assets\images\vue.png" alt="" /></div>
-              <div class="p-2"><img src="assets\images\nuxt.png" alt="" /></div>
-              <div class="p-2"><img src="assets\images\asp.jpg" alt="" /></div>
-              <div class="p-2">
-                <img src="assets\images\bootstrap.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\tailwind.png" alt="" />
-              </div>
-            </div>
-          </div>
-          <div
-            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
-          >
-            <h3 class="mb-4 text-2xl font-bold text-center">Backend</h3>
-            <div class="grid grid-cols-4">
-              <div class="p-2">
-                <img src="assets\images\express.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\laravel.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\codeigniter.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\csharp.png" alt="aa" />
+            <div
+              class="col-span-2 rounded-lg shadow-md p-6 mb-5 h-fit bg-gradient-to-r from-blue-400 to-blue-900"
+            >
+              <h3 class="mb-4 text-2xl font-bold text-center">Frontend</h3>
+              <div class="grid grid-cols-3">
+                <div class="p-2 w-[200px] h-[200px]">
+                  <img src="assets\images\vue.png" alt="" />
+                </div>
+                <div>
+                  <div class="p-2 w-[200px] h-[100px]">
+                    <img src="assets\images\nuxt.png" alt="" />
+                  </div>
+                  <div class="p-2 w-[200px] h-[100px]">
+                    <img src="assets\images\bootstrap.png" alt="" />
+                  </div>
+                </div>
+                <div class="p-2 w-[200px] h-[200px]">
+                  <img src="assets\images\tailwind.png" alt="" />
+                </div>
               </div>
             </div>
           </div>
 
-          <div
-            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
-          >
-            <h3 class="mb-4 text-2xl font-bold text-center">Database</h3>
-            <div class="grid grid-cols-5">
-              <div class="p-2">
-                <img src="assets\images\mysql.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\postgresql.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\mssql.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\oracle.png" alt="" />
-              </div>
-              <div class="p-2">
-                <img src="assets\images\mongodb.png" alt="" />
+          <div class="grid grid-cols-2 gap-8">
+            <div
+              class="rounded-lg shadow-md p-6 mb-5 h-fit bg-gradient-to-r from-blue-400 to-blue-900"
+            >
+              <h3 class="mb-4 text-2xl font-bold text-center">Backend</h3>
+              <div class="grid grid-cols-2">
+                <div class="p-2">
+                  <img src="assets\images\express.png" alt="" />
+                </div>
+                <div class="p-2">
+                  <img src="assets\images\laravel.png" alt="" />
+                </div>
+                <div class="p-2">
+                  <img src="assets\images\codeigniter.png" alt="" />
+                </div>
+                <div class="p-2">
+                  <img src="assets\images\csharp.png" alt="aa" />
+                </div>
               </div>
             </div>
-          </div>
-          <div
-            class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
-          >
-            <h3 class="mb-4 text-2xl font-bold text-center">Dev Ops</h3>
-            <div class="grid grid-cols-1">
-              <div class="p-2 mx-auto">
-                <img src="assets\images\docker.png" alt="" />
+            <div>
+              <div
+                class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
+              >
+                <h3 class="mb-4 text-2xl font-bold text-center">Database</h3>
+                <div class="grid grid-cols-5">
+                  <div class="p-2">
+                    <img src="assets\images\mysql.png" alt="" />
+                  </div>
+                  <div class="p-2">
+                    <img src="assets\images\postgresql.png" alt="" />
+                  </div>
+                  <div class="p-2">
+                    <img src="assets\images\mssql.png" alt="" />
+                  </div>
+                  <div class="p-2">
+                    <img src="assets\images\oracle.png" alt="" />
+                  </div>
+                  <div class="p-2">
+                    <img src="assets\images\mongodb.png" alt="" />
+                  </div>
+                </div>
+              </div>
+              <div
+                class="rounded-lg shadow-md p-6 mb-5 bg-gradient-to-r from-blue-400 to-blue-900"
+              >
+                <h3 class="mb-4 text-2xl font-bold text-center">Dev Ops</h3>
+                <div class="grid grid-cols-1">
+                  <div class="p-2 mx-auto">
+                    <img src="assets\images\docker.png" alt="" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -209,7 +238,9 @@
                   <div>Job desc</div>
                 </div>
                 <div class="col-span-2">
-                  <div>: Developed Management System for Property Developer</div>
+                  <div>
+                    : Developed Management System for Property Developer
+                  </div>
                 </div>
                 <div class="col-span-1">
                   <div>Technology</div>
@@ -242,7 +273,9 @@
                   <div>Job desc</div>
                 </div>
                 <div class="col-span-2">
-                  <div>: Developed Distribution Center (DC) for retail company</div>
+                  <div>
+                    : Developed Distribution Center (DC) for retail company
+                  </div>
                 </div>
                 <div class="col-span-1">
                   <div>Technology</div>
